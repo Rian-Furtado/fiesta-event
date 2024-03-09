c3291c797af15da2de0e9570b4b6e8de307cd263
Rian Furtado <rianoliveirafurtado@gmail.com>
2024-03-09 19:09:40 -0300

    Activity and Category

---
+++ src/main/java/com/event/house/entities/Category.java
@@ -0,0 +1,44 @@
+package com.event.house.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    List<Activity> activities = new ArrayList<>();
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
